01a2784cd13dc7f99b101c7e3445cae51b8755cc
Ludovic Dureau <ludodureau@gmail.com>
2020-06-24 18:21:21 +0200

    migrations fixed

--- migrations/Version20200624162048.php
+++ migrations/Version20200624162048.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200624160548 extends AbstractMigration
+final class Version20200624162048 extends AbstractMigration
 {
     public function getDescription() : string
     {
