6b70df688ded36d90471b99b4e970f8f4b8fa349
Carolinefourcade <caroannab33@gmail.com>
2020-06-25 12:09:20 +0200

    error sync

--- src/Controller/MedicalRecordController.php
+++ src/Controller/MedicalRecordController.php
@@ -4,16 +4,17 @@
 namespace App\Controller;
 
 
+use App\Repository\AnnoyanceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/medical", name="medical")
- * 
+ *
  */
 class MedicalRecordController extends AbstractController
 {
     public function index (AnnoyanceRepository $annoyanceRepository ) {
-        
+
     }
 }
