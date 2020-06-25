1b7ae986a30c7dcdc7867e048d3d596eb1232dcb
Carolinefourcade <caroannab33@gmail.com>
2020-06-25 14:43:38 +0200

    medical record ok

---
+++ src/Form/MedicalType.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\Annoyance;
+use App\Entity\AnnoyanceZone;
+use App\Entity\PainIntensity;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class MedicalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+       $builder
+           ->add('annoyanceZone', EntityType::class, [
+               'class'         => AnnoyanceZone::class,
+               'choice_label' => 'name'
+           ])
+           ->add('annoyance', EntityType::class, [
+               'class'         => Annoyance::class,
+               'choice_label' => 'name'
+           ])
+           ->add('painIntensity', EntityType::class, [
+               'class'         => PainIntensity::class,
+               'choice_label' => 'level'
+           ]);
+    }
+
+}
